ae2e7da667f230caa3005b054dda128d0960fd55
boming <boming0529@gmail.com>
2019-07-29 17:41:20 +0800

    [Add] dobtor_resource_contribution_ratio : add validation for ratio

--- dobtor_resource_contribution_ratio/__manifest__.py
+++ dobtor_resource_contribution_ratio/__manifest__.py
@@ -1,29 +1,14 @@
 # -*- coding: utf-8 -*-
 {
     'name': "dobtor_resource_contribution_ratio",
-
     'summary': """
-        Taiwan Labor insurance (Contribation ratio)
+        Taiwan Labor/Health insurance (Contribation ratio)
     """,
     'description': """
         Module for resource management.
-
-        Labor (Contribation ratio) : (https://www.bli.gov.tw/0021393.html)
-            - normal accident       (company - personal - government)
-                - Normal               70 - 20 - 10
-                - occupational unions   0 - 60 - 40
-                - fishermen             0 - 20 - 80
-            - occupational accident (company - personal - government)
-                - Normal              100 -  0 -  0
-                - occupational unions   0 - 60 - 40
-                - fishermen             0 - 20 - 80
-            - employment insurance
-                - Normal               70 - 20 - 10
-
-        Health (Contribation ratio) : 
-            
+            -Labor (Contribation ratio)
+            -Health (Contribation ratio)
     """,
-
     'author': "Dobtor SI",
     'website': "https://www.dobtor.com",
     'category': 'Uncategorized',
--- dobtor_resource_contribution_ratio/models/abstract_resource.py
+++ dobtor_resource_contribution_ratio/models/abstract_resource.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 
 class AbstractContributionRatio(models.AbstractModel):
     _name = 'abstract.resource.contribution_ratio'
 
-    def _get_default_start_date(self):
-        year = fields.Date.from_string(fields.Date.today()).strftime('%Y')
-        return '{}-01-01'.format(year)
+    # def _get_default_start_date(self):
+    #     year = fields.Date.from_string(fields.Date.today()).strftime('%Y')
+    #     return '{}-01-01'.format(year)
 
     name = fields.Char(
         string='Title',
@@ -26,9 +27,33 @@ class AbstractContributionRatio(models.AbstractModel):
         string='Contribution Ratio',
         compute='_compute_contribution_ratio',
     )
-    start_date = fields.Date(string='Start Date')
+    # start_date = fields.Date(string='Start Date')
 
     @api.depends('employer_ratio', 'employee_ratio', 'government_ratio')
     def _compute_contribution_ratio(self):
         for record in self:
-            record.contribution_ratio = '{}-{}-{}'.format(record.employer_ratio, record.employee_ratio, record.government_ratio)
+            record.contribution_ratio = '{:.0f}%-{:.0f}%-{:.0f}%'.format(
+                record.employer_ratio, record.employee_ratio, record.government_ratio)
+
+    @api.constrains('employer_ratio', 'employee_ratio', 'government_ratio')
+    def _check_ratio_validation(self):
+        """  validation at ratio create time. """
+        for record in self:
+            if record.employer_ratio > 100:
+                raise ValidationError(
+                    _("Employer Ratio has to be less then 100"))
+            if record.employer_ratio < 0.0:
+                raise ValidationError(
+                    _("Please enter Some Value for Employer Ratio"))
+            if record.employee_ratio > 100:
+                raise ValidationError(
+                    _("Employee Ratio has to be less then 100"))
+            if record.employee_ratio < 0.0:
+                raise ValidationError(
+                    _("Please enter Some Value for Employee Ratio"))
+            if record.government_ratio > 100:
+                raise ValidationError(
+                    _("Government Ratio has to be less then 100"))
+            if record.government_ratio < 0.0:
+                raise ValidationError(
+                    _("Please enter Some Value for Government Ratio"))
